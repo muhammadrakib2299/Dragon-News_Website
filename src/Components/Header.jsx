69f9ddbd43750aaf2fca1a297e95b7a4131928ba
rakib2299 <mdrakib@combosoft.co.uk>
2025-09-03 20:42:51 +0600

    added dynamic date

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,16 @@
 import logo from "../assets/logo.png";
+import moment from "moment";
 function Header() {
   return (
     <div className="flex flex-col justify-center items-center gap-2 py-2">
       <div className="logo py-2">
-        <img className="w-[300px]" src={logo} alt="" />
+        <img className="w-[350px]" src={logo} alt="" />
       </div>
       <div>
         <h1 className="text-gray-400">Journalism Without Fear or Favour</h1>
+      </div>
+      <div className="text-gray-400">
+        {moment().format("dddd, MMMM Do YYYY")}
       </div>
     </div>
   );
