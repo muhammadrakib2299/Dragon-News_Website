c52955dd0548c3eff37ca09bcbcddaa1a87d5b35
rakib2299 <mdrakib@combosoft.co.uk>
2025-09-10 10:45:22 +0600

    tring to fix

--- src/Layouts/PageLayout.jsx
+++ src/Layouts/PageLayout.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import { Outlet } from "react-router-dom";
-import Header from "../Components/Header";
 
 function PageLayout() {
   return (
     <div>
-      <header className="w-9/12 mx-auto py-2">
-        <Header></Header>
-        <Navbar></Navbar>
-      </header>
+      <Navbar> </Navbar>
       <Outlet></Outlet>
     </div>
   );
